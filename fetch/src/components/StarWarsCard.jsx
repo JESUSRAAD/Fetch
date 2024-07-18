9c76b5aa73469848a5f645e5b70f942efcc43f39
Jesus Raad <jesus.raad@students.gammatech.school>
2024-07-18 18:23:07 +0200

    me falta el nombre del planeta

--- fetch/src/components/StarWarsCard.jsx
+++ fetch/src/components/StarWarsCard.jsx
@@ -3,7 +3,7 @@ import React from 'react'
  const StarWarsCard = ({index, name,planet,height,mass}) => {
     // {key, img,name,planet,height,mass}
   return (
-    <div className='flex flex-col justify-around items-center w-32 min-w-max h-72 min-h-max rounded-xl bg-green-300 shadow-md'>
+    <div className='flex flex-col justify-evenly  items-center w-32 min-w-max h-72 min-h-max rounded-xl bg-green-300 shadow-md'>
         <div className='flex justify-center items-center w-[70%] h-[60%] border border-slate-950 '> <p className='text-black'>{index}</p></div>
         <div className="w-[90%] h-[30%] bg-neutral-200 text-center">
             <h2 className='text-xl'>{name}</h2>
